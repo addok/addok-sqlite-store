b1d758f87cd247f487a1cf9944303eab47efde5a
Yohan Boniface <yb@enix.org>
2017-03-16 18:59:45 +0100

    Fix tests following addok changes

--- tests/test_store.py
+++ tests/test_store.py
@@ -1,17 +1,20 @@
+import json
+
 from addok.batch import process_documents
 from addok.ds import DS, get_document
 
 
 def index_document(doc):
-    process_documents(doc)
+    process_documents(json.dumps(doc))
 
 
-def deindex_document(id_):
-    process_documents({'id': id_, '_action': 'delete'})
+def deindex_document(_id):
+    process_documents(json.dumps({'_id': _id, '_action': 'delete'}))
 
 
 doc = {
     'id': 'xxxx',
+    '_id': 'yyyy',
     'type': 'street',
     'name': ['Vernou-la-Celle-sur-Seine', 'Vernou'],
     'city': 'Paris',
@@ -23,7 +26,7 @@ doc = {
 def test_index_document():
     index_document(doc)
     with DS.conn as conn:
-        assert conn.execute('SELECT * from addok').fetchone()[0] == 'd|xxxx'
+        assert conn.execute('SELECT * from addok').fetchone()[0] == 'd|yyyy'
 
 
 def test_reindex_document_should_replace():
@@ -31,24 +34,24 @@ def test_reindex_document_should_replace():
     doc2 = doc.copy()
     doc2['name'] = 'Another name'
     index_document(doc2)
-    assert get_document(b'd|xxxx')['name'] == 'Another name'
+    assert get_document(b'd|yyyy')['name'] == 'Another name'
 
 
 def test_fetch_document():
     index_document(doc)
-    id_ = b'd|xxxx'
+    id_ = b'd|yyyy'
     assert list(DS.fetch(id_))[0][0] == id_
 
 
 def test_add_document():
-    data = ('d|xxxx', b'123')
+    data = ('d|yyyy', b'123')
     DS.upsert(data)
     with DS.conn as conn:
         assert conn.execute('SELECT * from addok').fetchone() == data
 
 
 def test_remove_document():
-    data = ('d|xxxx', b'123')
+    data = ('d|yyyy', b'123')
     DS.upsert(data)
     DS.remove(data[0])
     with DS.conn as conn:
@@ -57,6 +60,6 @@ def test_remove_document():
 
 def test_deindex_document():
     index_document(doc)
-    deindex_document(doc['id'])
+    deindex_document(doc['_id'])
     with DS.conn as conn:
         assert conn.execute('SELECT * from addok').fetchone() is None
